89aa0615d89a9f3a78e89734dc305b595a988554
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-08-20 22:04:38 +1000

    Create placeholder files for tax calculator

---
+++ src/js/calculateTax.ts
@@ -0,0 +1,39 @@
+import {businessName,
+businessSlogan,
+revenue,
+deductions,
+medicareLevy,
+companyTaxRate} from './data'
+
+// 1. Add up revenue from all months
+const addRevenue = () => {}
+
+// 2. Add up all allowable deducations
+const addDeductions = () => {} 
+
+// 3. Subtract total deductions from total revenue
+const calcTaxableIncome = () => {}
+
+// 4. Apply the company tax rate to taxable income
+const calcCompanyTaxRate = () => {}
+
+// 5. Calculate the medicare levy and add it to total tax owed
+const calcMedicareLevy = () => {}
+
+// 6. Output the result as total tax owed
+const calcTotalTaxOwed = () => {}
+
+
+addRevenue
+
+
+
+
+
+
+
+
+
+const adder = (x: number, y: number) => x + y
+
+export default adder
---
+++ src/js/data.js
@@ -0,0 +1,50 @@
+export const businessName = 'Tycho Space Station'
+
+export const businessSlogan = 'The hoopiest shipyard in the galaxy!'
+
+export const revenue = {
+  january: 25000,
+  february: 23500,
+  march: 28000,
+  april: 24800,
+  may: 36400,
+  june: 38300,
+  july: 29700,
+  august: 23500,
+  september: 26400,
+  october: 28500,
+  november: 23200,
+  december: 19500
+}
+
+export const deductions = {
+  darkMatterGenerators: 38000,
+  advertising: 15000,
+  medicalSupplies: 45000,
+  rocketFuel: 60000,
+  laserBlasters: 24000
+}
+
+export const medicareLevy = 1.5
+
+export const companyTaxRate = 30
+
+export const declareIndepenceAndRefuseToPayTaxes = false
+
+/*
+
+  Tax Tables circa 2019
+  0 - $18,200  Nil 
+  $18,201 – $37,000   19c for each $1 over $18,200
+  $37,001 – $90,000   $3,572 plus 32.5c for each $1 over $37,000
+  $90,001 – $180,000   $20,797 plus 37c for each $1 over $90,000
+  $180,001 and over   $54,097 plus 45c for each $1 over $180,000
+
+*/
+
+
+
+
+
+
+
