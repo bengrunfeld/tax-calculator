6933fe3f096d40b5844a44e7ed568c97d4c851ac
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-10-31 11:35:00 +1100

    Add return types on calc functions

--- src/js/calculateTax.ts
+++ src/js/calculateTax.ts
@@ -7,14 +7,14 @@ import {
 } from "../data/data.types"
 
 // 1. Add up revenue from all months
-const addRevenue = (revenue: Revenue) =>
+const addRevenue = (revenue: Revenue): number =>
   Object.keys(revenue).reduce((accum, item) => accum + revenue[item], 0)
 
 // 2. Add up all allowable deducations
 const addDeductions = (
   deductions: Deductions,
   fraudulentDeductions: FraudulentDeductions
-) => {
+): number => {
   const legit = Object.keys(deductions).reduce(
     (accum, item) => accum + deductions[item],
     0
@@ -28,20 +28,28 @@ const addDeductions = (
 }
 
 // 3. Subtract total deductions from total revenue
-const calcTaxableIncome = (totalRevenue: number, totalDeductions: number) =>
-  totalRevenue - totalDeductions
+const calcTaxableIncome = (
+  totalRevenue: number,
+  totalDeductions: number
+): number => totalRevenue - totalDeductions
 
 // 4. Apply the company tax rate to taxable income
-const calcCompanyTaxRate = (taxableIncome: number, corpTaxRate: number) =>
-  taxableIncome * corpTaxRate
+const calcCompanyTaxRate = (
+  taxableIncome: number,
+  corpTaxRate: number
+): number => taxableIncome * corpTaxRate
 
 // 5. Calculate the medicare levy and add it to total tax owed
-const calcMedicareLevy = (totalCompanyTax: number, medicareRate: number) =>
-  totalCompanyTax * medicareRate
+const calcMedicareLevy = (
+  totalCompanyTax: number,
+  medicareRate: number
+): number => totalCompanyTax * medicareRate
 
 // 6. Output the result as total tax owed
-const calcTotalTaxOwed = (totalCompanyTax: number, medicareLevy: number) =>
-  totalCompanyTax + medicareLevy
+const calcTotalTaxOwed = (
+  totalCompanyTax: number,
+  medicareLevy: number
+): number => totalCompanyTax + medicareLevy
 
 type CalculateTaxArgs = {
   corpTaxRate: CorpTaxRate
